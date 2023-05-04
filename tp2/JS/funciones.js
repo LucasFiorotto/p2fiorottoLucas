ec1539e11de7562e8b1ed5eada949548f4d1e1da
LucasFiorotto <129554123+LucasFiorotto@users.noreply.github.com>
2023-05-04 20:41:14 -0300

    Add files via upload

--- tp2/JS/funciones.js
+++ tp2/JS/funciones.js
@@ -1,10 +1,15 @@
 function consultar() {
-    fetch('https://rickandmortyapi.com/api/character')
-    // fetch('https://rickandmortyapi.com/api/character/1') "hardcodear"
+    var nconsulta = document.getElementById("consulta").value
+    fetch(`https://rickandmortyapi.com/api/character/${nconsulta}`)
     .then (function(response){
         return response.json()
     })
     .then (function(data){
-        console.log(data);
+        document.getElementById("info").innerHTML =`
+        <img src="${data.image}">
+        <p>${data.name}</p>
+        <p>${data.status}</p>
+        <p>${data.gender}</p>
+        <p>Aparece en ${data.episode.length} episodios.`
     })
 }
