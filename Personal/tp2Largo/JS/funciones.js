4195aa61501723afbe1d3c8d845030142afc2bff
LucasFiorotto <lucasfiorottotm@gmail.com>
2023-05-10 21:41:48 -0300

    cambios

--- Personal/tp2Largo/JS/funciones.js
+++ Personal/tp2Largo/JS/funciones.js
@@ -6,23 +6,25 @@ function consultar() {
         return response.json()
     })
     .then (function(data){
-        console.log(data);
-        document.getElementById("info").innerHTML =`
-        <img src="${data[0].image}">
-        <p>${data[0].name}
-        <p>${data[0].status}
-        <p>${data[0].gender}
-        <p>Aparece en ${data[0].episode.length} episodios.</p><br>
-        <img src="${data[1].image}"> 
-        <p>${data[1].name}</p>
-        <p>${data[1].status}</p>
-        <p>${data[1].gender}</p>
-        <p>Aparece en ${data[1].episode.length} episodios.</p>`
-        if (data[0].episode.length > data[1].episode.length) {
-            document.getElementById("masApariciones").innerHTML = `${data[0].name} tiene mas apariciones.`
+        if (nconsultaP1=="" || nconsultaP2=="") {
+            alert("Debe ingresar un valor en ambos campos.");
         } else {
-            document.getElementById("masApariciones").innerHTML = `${data[1].name} tiene mas apariciones.`
+            document.getElementById("info").innerHTML =`
+            <img src="${data[0].image}">
+            <p>${data[0].name}
+            <p>${data[0].status}
+            <p>${data[0].gender}
+            <p>Aparece en ${data[0].episode.length} episodios.</p><br>
+            <img src="${data[1].image}"> 
+            <p>${data[1].name}</p>
+            <p>${data[1].status}</p>
+            <p>${data[1].gender}</p>
+            <p>Aparece en ${data[1].episode.length} episodios.</p>`
+            if (data[0].episode.length > data[1].episode.length) {
+                document.getElementById("masApariciones").innerHTML = `${data[0].name} tiene mas apariciones.`
+            } else {
+                document.getElementById("masApariciones").innerHTML = `${data[1].name} tiene mas apariciones.`
+            }
         }
     })
 }
-// obligar al usuario a ingresar dos valores en los input (sin required)
